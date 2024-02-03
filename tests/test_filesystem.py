bec068e0a750235418f06dbd4dde3517af346ed7
Will Lachance <wlach@protonmail.com>
2024-02-03 17:22:43 -0500

    Parse repository with GitPython (#7)
    This allows us to ignore files in .gitignore.
    
    While we're here, add a Makefile with some targets to make local
    development easier.

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,6 +1,8 @@
 import tempfile
 from pathlib import Path, PurePath
 
+import git
+
 from repo_parser.filesystem import Dir, File, scan
 from repo_parser.processor import DEFAULT_PROCESSORS
 
@@ -10,11 +12,17 @@ def test_scan():
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdir = Path(tmpdir)
         (tmpdir / "README.md").write_text("This is a test")
+        (tmpdir / ".gitignore").write_text("ignored_dir\n")
+        (tmpdir / "ignored_dir").mkdir()
+        (tmpdir / "ignored_dir" / "README.md").write_text("This file should be ignored")
         (tmpdir / "service-example").mkdir()
         (tmpdir / "service-example" / "README.md").write_text(
             "---\ntype: service\nlanguage: python\n---\nThis is a service"
         )
         (tmpdir / "service-example" / "service.py").write_text("print('hello world')")
+        repo = git.Repo.init(tmpdir)
+        repo.index.add(["."])
+        repo.index.commit("initial commit")
 
         # scan the directory
         assert scan(tmpdir, DEFAULT_PROCESSORS) == Dir(
